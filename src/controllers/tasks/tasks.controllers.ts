10f0eda45905de0e6d4fb2d54d5dd9d946dddd57
ericzaim <eric.zaima@gmail.com>
2025-05-11 21:29:37 -0300

    adding CRUD opertations

--- src/controllers/tasks/tasks.controllers.ts
+++ src/controllers/tasks/tasks.controllers.ts
@@ -1,18 +1,126 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import Task from '../../entities/task.entity'
 export default class TaskController {
-  static async store (req:Request,res:Response) {
-    const { title, completed } = req.body
+  /**
+   * Adiciona Tasks
+   * @param req 
+   * @param res 
+   * @returns 
+   */
+  static async store (req:Request,res:Response, next: NextFunction){
 
+ const { title, completed, description,userID,time,isDaily } = req.body
     if (!title) {
-      return res.status(400).json({ error: 'O título é obrigatório' })
+      return res.status(400).json({ error: 'Nome da task é obrigatório' })
     }
 
     const task = new Task()
     task.title = title
+    task.description = description
     task.completed = completed || false
+    task.userID = userID
+    task.time = time
+    task.isDaily = isDaily
     await task.save()
 
     return res.status(201).json(task)
+}
+
+  static async index (req: Request, res: Response) {
+  const tasks = await Task.find()
+  return res.json(tasks)
+
+}
+/**
+ * Mostra tasks
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+
+  static async show (req:Request,res:Response){
+    const { id } = req.params
+
+    if(!id || id == ''){
+      return res.status(400).json({error:'ID is Required'})
+    }
+    const task = await Task.findOneBy({id:String(id)})
+    return res.json(task)
   }
+/**
+ * Deleta Tasks
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+    static async delete (req: Request, res: Response) {
+    const { id } = req.params
+
+    if(!id || id=='') {
+      return res.status(400).json({ error: 'ID is Required' })
+    }
+
+    const task = await Task.findOneBy({id: String(id)})
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' })
+    }
+
+    await task.remove()
+    return res.status(204).json()
+  }
+  /**
+   * Muda a task inteira
+   * @param req 
+   * @param res 
+   * @returns 
+   */
+    static async change (req: Request, res: Response) {
+    const { id } = req.params
+    const { title, completed } = req.body
+
+    if(!id || id == '') {
+      return res.status(400).json({ error: 'ID is Required' })
+    }
+
+    const task = await Task.findOneBy({id: String(id)})
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' })
+    }
+
+    task.title = title || task.title
+    task.completed = (completed === undefined) ? task.completed : completed
+    await task.save()
+
+    return res.json(task)
+  }
+  /**
+   * atualiza um atributo da task
+   * @param req 
+   * @param res 
+   * @returns 
+   */
+    static async update (req: Request, res: Response) {
+    const { id } = req.params
+    const { title, completed, description, time, isDaily } = req.body
+
+     if(!id || id == '') {
+      return res.status(400).json({ error: 'ID is Required' })
+    }
+
+    const task = await Task.findOneBy({id: String(id)})
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' })
+    }
+
+    task.title = title || task.title
+    task.completed = (completed === undefined) ? task.completed : completed
+    task.description = description || task.description
+    task.time = (task.time === time) ? task.time : task.time = time
+    task.isDaily = (task.isDaily === isDaily) ? task.isDaily : !task.isDaily
+
+    await task.save()
+
+    return res.json(task) // Vamos retornar a task atualizada
+  }
+  
 }
---
+++ src/controllers/users/users.controllers.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+export default class UsersController{
+    static async store (req:Request,res:Response){
+
+    }
+    static async find (req:Request,res:Response){
+
+    }
+}
