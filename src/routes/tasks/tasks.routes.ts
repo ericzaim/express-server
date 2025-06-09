73d287b5ab1e59e26fe37d8d7c1e49b645559a2e
ericzaim <eric.zaima@gmail.com>
2025-06-08 21:06:39 -0300

    fixing routes

--- src/routes/tasks/tasks.routes.ts
+++ src/routes/tasks/tasks.routes.ts
@@ -6,8 +6,11 @@ const tasksRoutes = Router()
 tasksRoutes.get('/', (req, res) => {TaskController.findAll(req, res)});
 tasksRoutes.get('/:id', (req, res) => {TaskController.findOneById(req, res)});
 tasksRoutes.get('/user/:userID', (req, res) => {TaskController.findByUser(req, res)});
-tasksRoutes.post('/', (req, res) => {TaskController.store(req, res) });
+
+tasksRoutes.post('/task', (req, res) => {TaskController.store(req, res) });
+
 tasksRoutes.delete('/:id', (req, res) => { TaskController.delete(req, res) });
+
 tasksRoutes.put('/:id',(req,res) => {TaskController.change(req, res)});
 tasksRoutes.patch('/:id',(req,res) => {TaskController.update(req,res) });
 
--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -3,14 +3,15 @@ import UsersController from '../../controllers/users/users.controllers'
 
 const usersRoutes = Router()
 
-usersRoutes.post('/', (req, res) => { UsersController.store(req, res) });
+usersRoutes.post('/cadastro', (req, res) => { UsersController.store(req, res) });
 
 usersRoutes.get('/', (req, res) => { UsersController.findAll(req, res) });
 usersRoutes.get('/:id', (req, res) => { UsersController.findOneById(req, res) });
 usersRoutes.get('/email/:email', (req, res) => { UsersController.findOneByEmail(req, res) });
 usersRoutes.get('/name/:name', (req, res) => { UsersController.findOneByName(req, res) });
 
-usersRoutes.patch('/:id', (req, res) => { UsersController.update(req, res) });
-usersRoutes.delete('/:id', (req, res) => { UsersController.delete(req, res) });
+usersRoutes.patch('/patch/:id', (req, res) => { UsersController.update(req, res) });
+
+usersRoutes.delete('/del/:id', (req, res) => { UsersController.delete(req, res) });
 
 export default usersRoutes
